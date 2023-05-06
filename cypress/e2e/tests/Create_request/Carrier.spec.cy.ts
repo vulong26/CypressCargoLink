37a6114f798691a9c9727b97a84de84bc093cae3
longvh22 <vulong265@gmail.com>
2023-05-06 14:05:00 +0700

    create rq

--- cypress/e2e/tests/Create_request/Carrier.spec.cy.ts
+++ cypress/e2e/tests/Create_request/Carrier.spec.cy.ts
@@ -3,6 +3,10 @@ import '../../../support/commands'
 import { carrierRequest } from '../../../pages/CarrierRequest.page';
 require('cypress-xpath');
 const carrier =new carrierRequest()
+const date = new Date()
+const startTime = new Date(date.getTime() + 2*(1000 * 60 * 60 * 24));
+const endTime = new Date(date.getTime() + 3*(1000 * 60 * 60 * 24));
+const expireTime = new Date(date.getTime() + (1000 * 60 * 60 * 24));
 describe('Login successful', () => {
     before('login', () => {
         cy.carrierLogin()
@@ -12,14 +16,25 @@ describe('Login successful', () => {
     });
     describe('fill data for request', () => {
         it('information page', () => {
-            carrier.fillContactInformation()
+            carrier.carrier.carNumber().type('90C-01872')
+            carrier.carrier.carNumberOpts().click()
+            carrier.carrier.weightCar().type('20')
+            carrier.carrier.deliverFee().type('1000000')
         });
 
         it('confirm request detail and expect fee', () => {
-           
+            carrier.carrier.lessTruckLoad().click()
+            carrier.carrier.startPoint().type('10, Phố Phạm Văn Bạch, Cầu Giấy, Hà Nội')
+            carrier.carrier.suggestOpts().click()
+            carrier.carrier.startTime().type(startTime.toLocaleDateString('en-GB') + '08:30')
+            carrier.carrier.endPoint().type('421 Xuân Đỉnh, Xuân Đỉnh, Bắc Từ Liêm, Hà Nội')
+            carrier.carrier.suggestOpts().click()
+            carrier.carrier.endTime().type(endTime.toLocaleString('en-GB') + '08:30')
+            carrier.carrier.expireTime().type(expireTime.toLocaleString('en-GB') + '08:30')
         });
         it('confirm request detail and expect fee', () => {
-            
+            carrier.clickToSend()
+            carrier.carrier.OKBtn().click()
         });
     })
 }); 
