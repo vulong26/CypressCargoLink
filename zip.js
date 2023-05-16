dc1b5f06e7d3d00ae43f24b0c034160f9adb86f2
longvh22 <vulong265@gmail.com>
2023-05-16 23:48:58 +0700

    abc

--- zip.js
+++ zip.js
@@ -1,6 +1,7 @@
 const AdmZip = require("adm-zip");
 
-var nodemailer =  require('nodemailer');
+var nodemailer = require('nodemailer');
+const fs = require('fs')
 
 async function createZipArchive() {
     const zip = new AdmZip();
@@ -10,31 +11,52 @@ async function createZipArchive() {
     console.log(`Created ${outputFile} successfully`);
 }
 
-createZipArchive()
-
-var transporter =  nodemailer.createTransport({ // config mail server
-    service: 'Gmail',
+var transporter = nodemailer.createTransport({ // config mail server
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: 'vulong.2662@gmail.com',
-        pass: 'VULONGLONG'
+        pass: 'jjkhanrrjgglnkum'
     }
 });
 
+const path = require("path")
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+    const files = fs.readdirSync(dirPath)
+  
+    arrayOfFiles = arrayOfFiles || []
+  
+    files.forEach(function(file) {
+      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+        arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+      } else {
+        arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+      }
+    })
+  
+    return arrayOfFiles
+  }
+
+const files = getAllFiles('./cypress/reports/html')
 var mainOptions = { // thiết lập đối tượng, nội dung gửi mail
     from: 'vulong265',
     to: 'je31828@lyqmeu.xyz',
     subject: 'Test Nodemailer',
-    text: 'You recieved message from ' + req.body.email,
-    attachments: [
-        {
-            filename: 'report.zip',
-            path: './html-reports.zip'
+    text: 'You recieved message from ',
+    attachments: files.map((v, i)=>{
+        return {
+            filename: v,
+            path: v
         }
-    ],
+    }),
     html: '<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>'
 }
 
-transporter.sendMail(mainOptions, function(err, info){
-    console.log(info)
-    console.log(err)
+transporter.sendMail(mainOptions, function (err, info) {
+    console.log(info);
+    console.log(
+        err
+    );
 });
